d0dead9f0bf27cb0d56d417c98cefab2a622aa34
Luke Cullen <75819270+retromonos@users.noreply.github.com>
2025-02-10 02:20:32 -0500

    test fix to editing

---
+++ LAMPAPI/GetContact.php
@@ -0,0 +1,73 @@
+<?php
+
+	$inData = getRequestInfo();
+	
+	$searchResults = "";
+	$searchCount = 0;
+
+	$conn = new mysqli("localhost", "dbuser", "POOST31g", "poosd31");
+	
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		
+		$stmt = $conn->prepare("select FirstName, LastName, Phone, Email, Description, ID from Contacts where ID=?");
+		$colorName = "%" . $inData["search"] . "%";
+		
+		$stmt->bind_param("i", $colorName, $colorName, $inData["id"]);
+		
+		$stmt->execute();
+		
+
+		$result = $stmt->get_result();
+		
+		while($row = $result->fetch_assoc())
+		{
+			if( $searchCount > 0 )
+			{
+				$searchResults .= ",";
+			}
+			$searchCount++;
+			$searchResults .= '{"firstName": "' . $row["FirstName"] . '", "lastName": "' . $row["LastName"] . '", "phone": "'.$row["Phone"].'", "email": "'.$row["Email"].'", "description": "'.$row["Description"].'", "id": '.$row["ID"].'}';
+		}
+		
+		if( $searchCount == 0 )
+		{
+			returnWithError( "No Records Found again" );
+		}
+		else
+		{
+			returnWithInfo( $searchResults );
+		}
+		
+		$stmt->close();
+		$conn->close();
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"id":'.$inData["userId"].',"firstName":"","lastName":"","error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+	function returnWithInfo( $searchResults )
+	{
+		$retValue = '{"results":[' . $searchResults . '],"error":""}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
